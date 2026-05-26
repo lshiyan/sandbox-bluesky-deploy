ee3c795915288d51d05ae57bbe2ce0cf0d16595f
Shiyan <shiyan.wliu@gmail.com>
2026-05-26 15:08:29 -0400

    Domain name change

--- infra/constructs/compute.ts
+++ infra/constructs/compute.ts
@@ -148,8 +148,8 @@ export class Compute extends Construct {
     });
 
     // PDS will verify users at endpoints like:
-    // https://<userhandle>.sandboxsocial3.net/.well-known/atproto-did
-    // This wildcard record will route *.sandboxsocial3.net to the ALB
+    // https://<userhandle>.socialsandbox3.net/.well-known/atproto-did
+    // This wildcard record will route *.socialsandbox3.net to the ALB
     new route53.ARecord(this, 'WildcardDNS', {
       zone: props.network.hostedZone,
       recordName: `*.${props.domainName}`,
